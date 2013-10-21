1db6b1540f98053c87ff5e1a7df1d311a236014c
Eric Pagendarm <Eric.Pagendarm@gmail.com>
2013-10-20 19:09:21 -0700

    Updated with newest revison. Evaluator and HFSM updated

--- src/pacman/Executor.java
+++ src/pacman/Executor.java
@@ -68,13 +68,13 @@ public class Executor
 		
 		// run unit tests during execution
 		boolean bRunUnitTests=true;
-		//exec.runGameTimed(new NearestPillPacMan(),new AggressiveGhosts(),visual);
+//		exec.runGameTimed(new NearestPillPacMan(),new AggressiveGhosts(),visual);
 
 		//exec.runGameTimed(new StarterPacMan(),new EvaluationAgent(),visual);
 		//exec.runGameTimed(new StarterPacMan(),new MyGhosts(),visual,bRunUnitTests);
-		//exec.runGameTimed(new HumanController(new KeyBoardInput()),new FsmGhosts(),visual,bRunUnitTests);
+		exec.runGameTimed(new HumanController(new KeyBoardInput()),new MyGhosts(),visual,bRunUnitTests);
 
-		exec.runGameTimed(new HumanController(new KeyBoardInput()),new StarterGhosts(),visual, bRunUnitTests);	
+//		exec.runGameTimed(new HumanController(new KeyBoardInput()),new StarterGhosts(),visual);	
 		//*/
 		
 		/*
@@ -170,6 +170,7 @@ public class Executor
     public void runGameTimed(Controller<MOVE> pacManController,Controller<EnumMap<GHOST,MOVE>> ghostController,boolean visual,boolean bRunUnitTests)
 	{
 		Game game=new Game(0);
+		Evaluator eval = new Evaluator();
 		
 		GameView gv=null;
 		
@@ -202,7 +203,7 @@ public class Executor
 	        	gv.repaint();
 	        
 	        if (bRunUnitTests)
-	        	Evaluator.runUnitTests(game,pacManController,ghostController);
+	        	eval.runUnitTests(game,pacManController,ghostController);
 
 		}
 		
